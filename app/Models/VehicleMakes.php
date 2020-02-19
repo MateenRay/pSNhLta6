6a98afc1e9619221ab1f553ebb54bbff3c55f62a
Mateen Ray <mateen@Mateen-Ray-MacBook-Pro.local>
2020-02-19 23:10:39 +0530

    points 1 to 4 added 2020-02-19 23:10:39

--- app/Models/VehicleMakes.php
+++ app/Models/VehicleMakes.php
@@ -6,4 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class VehicleMakes extends Model {
   protected $guarded = ['id'];
+
+  public function vehicleModels()
+  {
+    return $this->hasMany(VehicleMakes::class);
+  }
 }
--- app/Models/VehicleModels.php
+++ app/Models/VehicleModels.php
@@ -6,4 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class VehicleModels extends Model {
   protected $guarded = ['id'];
+
+  public function vehicles()
+  {
+    return $this->belongsTo(VehicleModels::class);
+  }
 }
